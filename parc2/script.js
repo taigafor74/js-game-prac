cfb333c3247d2763760fc32118e206f06c6ce559
kwn74 <11428169+kwn74@user.noreply.gitee.com>
2022-11-08 13:38:27 +0800

    optimize prac2

--- parc2/script.js
+++ parc2/script.js
@@ -21,7 +21,6 @@ class Layer {
     this.y = 0;
     this.width = 2400;
     this.height = 700;
-    this.x2 = this.width;
     this.image = image;
     this.speedModifier = speedModifier;
     this.speed = gameSpeed * this.speedModifier;
@@ -29,17 +28,19 @@ class Layer {
   updated() {
     this.speed = gameSpeed * this.speedModifier;
     if (this.x <= -this.width) {
-      this.x = this.width + this.x2 - this.speed;
-    }
-    if (this.x2 <= -this.width) {
-      this.x2 = this.width + this.x - this.speed;
+      this.x = 0;
     }
     this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
   }
   draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 const layer1 = new Layer(backgroundLayer1, 0.5);
@@ -47,7 +48,8 @@ const layer2 = new Layer(backgroundLayer2, 0.5);
 const layer3 = new Layer(backgroundLayer3, 0.5);
 const layer4 = new Layer(backgroundLayer4, 0.5);
 const layer5 = new Layer(backgroundLayer5, 1);
-const gameObj = [layer1, layer2, layer3, layer4, layer5];
+// const gameObj = [layer1, layer2, layer3, layer4, layer5];
+const gameObj = [layer4];
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   gameObj.forEach((item) => {
